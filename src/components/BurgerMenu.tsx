f775d231fbd2cb177aa999574955625ba129e7e8
HaillSeitan <150841231+HaillSeitan@users.noreply.github.com>
2024-07-03 12:02:30 +0200

    on click change burger icon to x mark

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -1,24 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 function BurgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenueToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="max-sm:block max-2xl:hidden stroke-amber-400 hover:text-amber-600 transform hover:shadow-sm">
       <Menu>
-        <MenuButton>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            className="size-8"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
+        <MenuButton onClick={handleMenueToggle}>
+          {isOpen ? (
+            <XMarkIcon className="size-8 text-amber-400" />
+          ) : (
+            <Bars3Icon className="size-8 text-amber-400" />
+          )}
         </MenuButton>
         <MenuItems
           className="bg-stone-800/90 text-amber-100 w-52 h-screen mt-4"
