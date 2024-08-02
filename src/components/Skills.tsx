d5564ba5c5639402644d0812791184621160b7dc
HaillSeitan <150841231+HaillSeitan@users.noreply.github.com>
2024-08-02 12:52:46 +0200

    add skills description

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -61,25 +61,48 @@ function Skills() {
           Skills
           <span className="text-amber-600">/&gt;</span>
         </h2>
-        <div className="flex flex-col gap-6">
-          {skillsData.map((category, index) => (
-            <AnimatedDiv key={index} direction="left">
-              <div className="bg-stone-400 bg-opacity-20 backdrop-blur-lg rounded-xl p-6 shadow-lg">
-                <p className="font-titillium text-xl text-amber-50 mb-4">
-                  {category.category}
-                </p>
-                <div className="flex flex-wrap gap-6">
-                  {category.skills.map((skill, skillIndex) => (
-                    <SkillsItem
-                      key={skillIndex}
-                      logo={skill.logo}
-                      title={skill.title}
-                    />
-                  ))}
+        <div className="flex flex-col lg:flex-row gap-6">
+          <div className="flex flex-col gap-6 lg:w-3/4">
+            {skillsData.map((category, index) => (
+              <AnimatedDiv key={index} direction="left">
+                <div className="bg-stone-400 bg-opacity-20 backdrop-blur-lg rounded-xl p-6 shadow-lg">
+                  <p className="font-titillium text-xl text-amber-50 mb-4">
+                    {category.category}
+                  </p>
+                  <div className="flex flex-wrap gap-6">
+                    {category.skills.map((skill, skillIndex) => (
+                      <SkillsItem
+                        key={skillIndex}
+                        logo={skill.logo}
+                        title={skill.title}
+                      />
+                    ))}
+                  </div>
                 </div>
-              </div>
-            </AnimatedDiv>
-          ))}
+              </AnimatedDiv>
+            ))}
+          </div>
+          <div className="lg:w-1/4 bg-stone-400 bg-opacity-20 backdrop-blur-lg rounded-xl p-6 shadow-lg text-orange-50 flex flex-col gap-4">
+            <p>
+              As a web developer specializing in frontend development and
+              design, I excel in creating responsive, visually appealing web
+              applications using HTML, CSS, and JavaScript. With TypeScript, I
+              ensure robust and maintainable code. My expertise includes
+              building modern interfaces with React and Next.js, and efficiently
+              styling with Tailwind CSS.
+            </p>
+            <p>
+              Additionally, I have foundational knowledge in backend
+              development, including setting up server-side applications with
+              Node.js, managing databases with Prisma, and containerization
+              using Docker.
+            </p>
+            <p>
+              I utilize tools like VS Code for coding, Git and GitHub for
+              version control, and Figma for design, which streamline my
+              workflow and enhance productivity.
+            </p>
+          </div>
         </div>
       </div>
     </section>
